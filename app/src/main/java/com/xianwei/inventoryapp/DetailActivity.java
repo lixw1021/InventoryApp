6e2d237d1ad0ba653b945e2fee245506a6ef5edb
xianwei li <xil148@pitt.edu>
2017-08-19 23:54:19 -0400

    Initial commit

---
+++ app/src/main/java/com/xianwei/inventoryapp/DetailActivity.java
@@ -0,0 +1,17 @@
+package com.xianwei.inventoryapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by xianwei li on 8/19/2017.
+ */
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
---
+++ app/src/main/java/com/xianwei/inventoryapp/data/ProductContract.java
@@ -0,0 +1,8 @@
+package com.xianwei.inventoryapp.data;
+
+/**
+ * Created by xianwei li on 8/19/2017.
+ */
+
+public class ProductContract {
+}
