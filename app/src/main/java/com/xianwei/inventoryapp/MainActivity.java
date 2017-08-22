3de76d41a7725ed4055bb236c8436c8372bd73d1
xianwei li <xil148@pitt.edu>
2017-08-22 17:57:03 -0400

    updated ui, add butterknife and floating action button

--- app/src/main/java/com/xianwei/inventoryapp/MainActivity.java
+++ app/src/main/java/com/xianwei/inventoryapp/MainActivity.java
@@ -1,14 +1,30 @@
 package com.xianwei.inventoryapp;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.floating_add)
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
+    }
+
+    @OnClick(R.id.floating_add)
+    public void addItem(View view) {
+        Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+        startActivity(intent);
     }
 }
