54744569f950b052afaa1cf3d8d2880a4c75c0dd
xianwei li <xil148@pitt.edu>
2017-08-25 11:14:32 -0400

    add delete all menu in main activity

--- app/src/main/java/com/xianwei/inventoryapp/MainActivity.java
+++ app/src/main/java/com/xianwei/inventoryapp/MainActivity.java
@@ -10,6 +10,8 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 
@@ -56,6 +58,23 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public void addItem(View view) {
         Intent intent = new Intent(MainActivity.this, EditActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.delete_all:
+                getContentResolver().delete(ProductEntry.CONTENT_URI, null, null);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
